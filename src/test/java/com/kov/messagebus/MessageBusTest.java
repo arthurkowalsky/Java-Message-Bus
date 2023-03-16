845d297d53fb7f9a571b80782c00409f2213df8c
Artur Kowalski <artur.kowalski@thecoders.io>
2023-03-16 10:04:09 +0100

    1.0.0 Message Bus

--- src/test/java/com/kov/messagebus/MessageBusTest.java
+++ src/test/java/com/kov/messagebus/MessageBusTest.java
@@ -3,8 +3,7 @@ package com.kov.messagebus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MessageBusTest {
     private MessageBus messageBus;
@@ -25,6 +24,43 @@ class MessageBusTest {
     @Test
     void testInvokeWithoutHandler() {
         assertThrows(RuntimeException.class, () -> messageBus.invoke(new TestMessage("Hello, world!")));
+    }
+
+    @Test
+    public void testRegisterHandlerThrowsExceptionOnHandlerCreationFailure() {
+        Class<?> handlerClass = BrokenTestMessageHandler.class;
+
+        Exception exception = assertThrows(RuntimeException.class, () -> messageBus.registerHandler(handlerClass));
+        assertTrue(exception.getMessage().contains("Failed to register handler"));
+    }
+
+    @Test
+    public void testInvokeThrowsExceptionWhenHandlerInvocationFails() {
+        messageBus.registerHandler(FailingTestMessageHandler.class);
+
+        TestMessage testMessage = new TestMessage("hello world");
+
+        Exception exception = assertThrows(RuntimeException.class, () -> messageBus.invoke(testMessage));
+        assertTrue(exception.getMessage().contains("Failed to invoke handler for message"));
+    }
+
+    @MessageHandler
+    static class BrokenTestMessageHandler {
+        public BrokenTestMessageHandler() throws Exception {
+            throw new Exception("Failed to create the handler");
+        }
+
+        public Void invoke(TestMessage message) {
+            // This method will not be called since the constructor fails.
+            return null;
+        }
+    }
+
+    @MessageHandler
+    static class FailingTestMessageHandler {
+        public Void invoke(TestMessage message) {
+            throw new RuntimeException("Failed to invoke the handler");
+        }
     }
 
     @MessageHandler
